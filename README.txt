ccc93f415bd416a4408b406722318033cb03b522
estebanmatias92 <estebanmatias92@gmail.com>
2012-09-11 01:22:51 -0300

    Damnatio Master FInal Changes

---
+++ README.txt
@@ -0,0 +1,18 @@
+Damnatio HTML5 & CSS3 Template.
+
+---
+
+Template Made entirely in HTML5 and CSS3, a proof that 
+it is completely possible use the more advanced concepts 
+of these technologies in current browsers, of course, except IE. 
+Despite this latter have total and complete support in all other browsers.
+
+Features:
+
+- Full design on CSS3.
+- HTML5 document structure.
+- Roles and interaction with jQuery.
+- Optimized loading rate (CDN usage, and images optimized).
+- Nivo-Slider, personal modified.
+- HTML5 Contact Form.
+- Less for CSS preprocessing.
--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,5 @@
 // Functions
 
-// Menu Bar current function
-/*$(document).ready(function(){
-    var url_complete = location.href; //URL de la pagina actual
-    var url_start = url_complete.lastIndexOf("/");
-    var current_page = url_complete.slice(url_start+1); // Extraemos el nombre de la pagina
-    $("nav ul li a[href='"+ current_page +"']").addClass("active"); //Asignamos la clase llamada "activo"
-});*/
-
-// Nav animation
-/*$(document).ready(function(){
-	var path = location.pathname.substring(1);
-	if ( path )
-		$('nav ul li a[href$="' + path + '"]').attr('class', 'nav ul li a.current');
-		$('nav ul li a[href$="' + path + '"]').hover(function(){
-			$(this).addclass("nav ul li a.current");
-		});
-});*/
-
-//NivoSlider ThumbNails effect
-/*$(document).ready(function(){
-	$("#slider article a img").fadeTo("slow", 0.3);
-	$("#slider article a img").hover(function(){
-		$(this).fadeTo("slow", 1.0);
-	},function(){
-			$(this).fadeTo("slow", 0.3);
-	});
-});*/
-
 // SocialMedia buttons animated
 $(document).ready(function() {
     $('#socialmedia li a').hover(function() {
